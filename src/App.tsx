9dd3a93a9241daa3693817b985f823842acad0c7
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-10-12 12:16:28 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <main>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Exercitationem, nobis distinctio voluptatibus accusamus maxime dicta
-        omnis illo modi iusto expedita pariatur tempora dolorem ipsa dolores
-        suscipit ducimus accusantium soluta fugit!
-      </h1>
+      <Header />
+      <div>
+        <div></div>
+      </div>
     </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="w-full">
+      <nav>
+        <a href="#">
+          <img src="./images/logo.png" alt="logo" className="logo" />
+        </a>
+        <a href="#" className="btn radius">
+          Buy Now
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
