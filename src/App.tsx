834a2c1924718801b22e7af97a10ed128e3c510c
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-10-12 11:54:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Exercitationem, nobis distinctio voluptatibus accusamus maxime dicta
+        omnis illo modi iusto expedita pariatur tempora dolorem ipsa dolores
+        suscipit ducimus accusantium soluta fugit!
+      </h1>
+    </main>
+  );
+}
+
+export default App;
